ea403d48682fa0783a586954c6d694ac4fcadf13
Michael Black <michaelpblack16@gmail.com>
2020-05-11 10:50:18 -0500

    Delete go.zsh

--- oh-my-zsh/custom/go.zsh
+++
@@ -1,8 +0,0 @@
-# For specific versions of go installed with homebrew
-# export PATH=$PATH:/usr/local/opt/go@1.9/libexec/bin
-
-# For default go installation
-# export PATH=$PATH:/usr/local/go/bin
-
-export GOPATH=$HOME/go
-export PATH=$PATH:$GOPATH/bin
