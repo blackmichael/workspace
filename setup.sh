7a28a17bb1296ec6f12195098af74a807aea9cb8
Michael Black <michaelpblack16@gmail.com>
2020-05-11 10:53:28 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,32 +14,20 @@ sed -E 's/ZSH_THEME="[a-zA-Z\-_]+"/ZSH_THEME="gangly"/' $HOME/.zshrc
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install homebrew packages
-brew cask install caffeine
-brew cask install firefox
-brew cask install google-chrome
-brew cask install iterm2
-brew cask install slack
-brew cask install skype
-brew cask install spectacle
-brew cask install spotify
-brew cask install java
-brew cask install intellij-idea
-brew cask install postman
-brew install dockutil
-brew install gpg
-brew install jq
-brew install wget
-brew install go
+# brew cask install caffeine
+# brew cask install firefox
+# brew cask install google-chrome
+# brew cask install iterm2
+# brew cask install slack
+# brew cask install skype
+# brew cask install java
+# brew cask install intellij-idea
+# brew cask install postman
+# brew install dockutil
+# brew install gpg
+# brew install jq
+# brew install wget
+# brew install go
 
 # Clean up the dock
 # dockutil --remove
-
-# Install sdkman
-curl -s "https://get.sdkman.io" | bash
-
-# Install sdks
-source "/Users/mblack/.sdkman/bin/sdkman-init.sh"
-sdk install gradle
-sdk install groovy
-sdk install kotlin
-sdk install kscript
